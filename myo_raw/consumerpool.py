efd54c47d413c38808457697dc1ca8aaa23ac09e
Matthias Gazzari <mail@qtux.eu>
2018-09-08 03:24:12 +0200

    Fix and improve ConsumerPool
    Join threads on calling pop_callback and clear_callback instead of
    joining all threads on calling shutdown to avoid race conditions.
    Additionally, fix the license header and remove the unused timeout
    argument in the shutdown method.

--- myo_raw/consumerpool.py
+++ myo_raw/consumerpool.py
@@ -1,5 +1,5 @@
 #
-# Original work Copyright (c) 2018 Matthias Gazzari
+# Copyright (c) 2018 Matthias Gazzari
 #
 # Licensed under the MIT license. See the LICENSE file for details.
 #
@@ -18,7 +18,7 @@ class ConsumerPool():
         '''
         self._queues = {category: [] for category in data_categories}
         self._callbacks = {category: [] for category in data_categories}
-        self._threads = []
+        self._threads = {category: [] for category in data_categories}
         self._sentinel = object()
 
     def add_callback(self, data_category, consumer_callback):
@@ -38,7 +38,7 @@ class ConsumerPool():
                 data = data_queue.get()
 
         thread = threading.Thread(target=run_consumer)
-        self._threads.append(thread)
+        self._threads[data_category].append(thread)
         thread.start()
 
     def pop_callback(self, data_category, index=-1):
@@ -49,6 +49,7 @@ class ConsumerPool():
         :param returns: the removed callback function
         '''
         self._queues[data_category].pop(index).put(self._sentinel)
+        self._threads[data_category].pop(index).join()
         return self._callbacks[data_category].pop(index)
 
     def clear_callbacks(self, data_category):
@@ -58,8 +59,11 @@ class ConsumerPool():
         '''
         for data_queue in self._queues[data_category]:
             data_queue.put(self._sentinel)
+        for thread in self._threads[data_category]:
+            thread.join()
         self._queues[data_category].clear()
         self._callbacks[data_category].clear()
+        self._threads[data_category].clear()
 
     def enqueue_data(self, data_category, *data):
         '''Enqueue data of a given data category to be processed by corresponding callbacks.
@@ -70,13 +74,12 @@ class ConsumerPool():
         for data_queue in self._queues[data_category]:
             data_queue.put(data)
 
-    def shutdown(self, timeout=None):
+    def shutdown(self):
         '''Stop consuming new data and wait up to timeout seconds for all threads to terminate.
-
-        :param timeout: the maximum amount of time to wait for the consumer threads to terminate
         '''
         for queue_list in self._queues.values():
             for data_queue in queue_list:
                 data_queue.put(self._sentinel)
-        for thread in self._threads:
-            thread.join(timeout)
+        for thread_list in self._threads.values():
+            for thread in thread_list:
+                thread.join()
